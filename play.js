074e1923f20367ecaae00f5579fa4bc99fbcaf37
Christina  Matson <cmatson93@gmail.com>
2018-01-21 22:31:36 -0800

    Color change on Hit

--- play.js
+++ play.js
@@ -27,7 +27,7 @@ for (var i = 0; i < gameBoard.length; i++) {
     row = $('<tr />');
     table.append(row);
     for (var j = 0; j < gameBoard[i].length; j++) {
-        cell = $('<td>' + gameBoard[i][j] + '</td>');
+        cell = $('<td value=' + gameBoard[i][j] + '>' + gameBoard[i][j] + '</td>');
         row.append(cell);
     }
 }
@@ -36,3 +36,33 @@ for (var i = 0; i < gameBoard.length; i++) {
 //Ask where?
 //Start point, direction 
 //(L,X,Y,D) =>
+
+$("td").click(function() {
+    console.log("clicked")
+    console.log($(this).attr("value"));
+    boat = ($(this).attr("value"));
+    switch (boat) {
+        case "2":
+            console.log("HIT A 2");
+            $(this).addClass("hit");
+            break;
+        case "3":
+            console.log("HIT A 3");
+            $(this).addClass("hit");
+            break;
+        case "4":
+            console.log("HIT A 4");
+            $(this).addClass("hit");
+            break;
+        case "5":
+            console.log("HIT A 5");
+            $(this).addClass("hit");
+            break;
+    }
+    // if ($(this).attr("value") == 2) {
+    //     console.log($(this).attr("value"));
+    //     console.log("Hit");
+    //     $(this).addClass("hit");
+    // }
+
+})
